86e748b25a38d5026a944ee57f05436682a37efc
Michal Ambrozewicz <michal.ambrozewicz@gmail.com>
2019-05-14 20:22:40 +0200

    Add map function to rendering HTML in Summary.js

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,52 +1,22 @@
 import React from "react";
 
-const Summary = () => {
+const Summary = props => {
+  const summary = Object.keys(props.form).map(item =>
+    props.form[item] !== "" ? (
+      <tr key={item}>
+        <th>From</th>
+        <td>{props.form[item]}</td>
+      </tr>
+    ) : null
+  );
+
   return (
     <div className="summary">
       <div className="summary-info">
         <h1 className="summary-header">Your booking</h1>
         <table className="summary-details">
           <tbody>
-            <tr>
-              <th>From</th>
-              <td>Hilton Gdansk, Targ Rybny 1, 80-838 Gdańsk, Poland</td>
-            </tr>
-            <tr>
-              <th>To</th>
-              <td>Berlin Brandenburg Airport (BER)</td>
-            </tr>
-            <tr>
-              <th>Date</th>
-              <td>Tuesday, 14 May 2019</td>
-            </tr>
-            <tr>
-              <th>Pickup Time</th>
-              <td>Distance</td>
-            </tr>
-            <tr>
-              <th>Estimated duration</th>
-              <td>6:35</td>
-            </tr>
-            <tr>
-              <th>Passengers</th>
-              <td>2</td>
-            </tr>
-            <tr>
-              <th>Children seats</th>
-              <td>2</td>
-            </tr>
-            <tr>
-              <th>Luggage</th>
-              <td>2</td>
-            </tr>
-            <tr>
-              <th>Sport luggage</th>
-              <td>1</td>
-            </tr>
-            <tr>
-              <th>Animals</th>
-              <td>1</td>
-            </tr>
+            {summary}
           </tbody>
         </table>
       </div>
