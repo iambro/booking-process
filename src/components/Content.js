2665a183b6966871686d0577ffd7f071932a41ce
Michal Ambrozewicz <michal.ambrozewicz@gmail.com>
2019-05-14 17:41:27 +0200

    Move functions and state from ContenBox to Content

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,13 +4,51 @@ import { ContentBox } from "./ContentBox";
 import { Summary } from "./Summary";
 
 class Content extends Component {
-  state = {};
+  state = {
+    form: {
+      from: "",
+      to: "",
+      date: "",
+      pickupTime: "",
+      distance: "",
+      estimatedDuration: "",
+      passengers: "",
+      childrenSeats: "",
+      luggage: "",
+      sportLuggage: "",
+      animals: "",
+      voucherCode: ""
+    }
+  };
+
+  handleInputs = e => {
+    let inputName = e.target.name;
+    let inputValue = e.target.value;
+    let statusCopy = Object.assign({}, this.state);
+    statusCopy.form[inputName] = inputValue;
+    this.setState(statusCopy);
+  };
+
   render() {
+    // const {
+    //   from,
+    //   to,
+    //   date,
+    //   pickupTime,
+    //   distance,
+    //   estimatedDuration,
+    //   passengers,
+    //   childrenSeats,
+    //   luggage,
+    //   sportLuggage,
+    //   animals,
+    //   voucherCode
+    // } = this.state.form;
     return (
       <div className="content">
         <Steps />
-        <ContentBox />
-        <Summary />
+        <ContentBox handleInputs={this.handleInputs} />
+        <Summary form={this.state.form} />
       </div>
     );
   }
