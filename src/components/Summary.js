28d8447d81d52301911aed5d35a0203d3776d86b
Michal Ambrozewicz <michal.ambrozewicz@gmail.com>
2019-05-15 12:22:15 +0200

    Add condition to Summary.js

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Summary = props => {
-  const summary = Object.keys(props.form).map(item =>
-    props.form[item] !== "" ? (
+  const summaryList = Object.keys(props.form).map(item =>
+    props.form[item] !== 0 && props.form[item] !== "" ? (
       <tr key={item}>
         <th>{props.summary[item]}</th>
         <td>{props.form[item]}</td>
@@ -15,7 +15,7 @@ const Summary = props => {
       <div className="summary-info">
         <h1 className="summary-header">{props.summary.your}</h1>
         <table className="summary-details">
-          <tbody>{summary}</tbody>
+          <tbody>{summaryList}</tbody>
         </table>
       </div>
       <div className="summary-map" />
